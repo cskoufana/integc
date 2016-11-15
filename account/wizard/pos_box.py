f1ac4972e9bfec6b36089ceff75e48eb6e8ed574
Marius YENKE MBEUYO <yenkemarius@yahoo.fr>
2016-11-15 11:03:50 +0100

    Ajout du partenaire sur le wizard de retrait d'argent Ajout du produit sur le wizard de retrait d'argent

--- account/wizard/pos_box.py
+++ account/wizard/pos_box.py
@@ -1,6 +1,7 @@
 #__author__ = 'yenke'
 from openerp.osv import fields, osv
 import time
+from openerp.tools.translate import _
 
 
 class CashBoxIn(osv.osv_memory):
@@ -24,11 +25,23 @@ class CashBoxOut(osv.osv_memory):
 
     _columns = {
         'analytic_account_id': fields.many2one('account.analytic.account', 'Analytic account'),
+        'product_id': fields.many2one('product.product', 'Product'),
+        'partner_id': fields.many2one('res.partner', 'Partner')
     }
 
     def _compute_values_for_statement_line(self, cr, uid, box, record, context=None):
         res = super(CashBoxOut, self)._compute_values_for_statement_line(cr, uid, box, record, context=context)
         res['analytic_account_id'] = box.analytic_account_id.id if box.analytic_account_id else False,
         res['date'] = time.strftime('%Y-%m-%d %H:%M:%S')
+        res['partner_id'] = box.partner_id.id if box.partner_id else False,
+        if box.product_id:
+            account_id = box.product_id.property_account_income.id
+            if not account_id:
+                account_id = box.product_id.categ_id.property_account_income_categ.id
+            if not account_id:
+                raise osv.except_osv(_('Error!'),
+                        _('Please define income account for this product: "%s" (id:%d).') % \
+                            (box.product_id.name, box.product_id.id,))
+            res['account_id'] = account_id
         return res
 
